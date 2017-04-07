a4ee82dbaaab144b0eeba6e26fa12af68d94265b
Tanner Sawyer <tanner.sawyer@gmail.com>
2017-04-07 14:15:55 -0600

    add category_id to serializer

--- app/serializers/account_transaction_serializer.rb
+++ app/serializers/account_transaction_serializer.rb
@@ -1,3 +1,3 @@
 class AccountTransactionSerializer < ActiveModel::Serializer
-  attributes :id, :transaction_id, :amount, :transaction_type, :date, :pending, :pending_transaction_id, :address, :city, :state, :zip, :lat, :lon, :name
+  attributes :id, :transaction_id, :amount, :transaction_type, :date, :pending, :pending_transaction_id, :address, :city, :state, :zip, :lat, :lon, :name, :category_id
 end
