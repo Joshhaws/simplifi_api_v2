bf5aaa15dd7dc77efb13f7386d8e29f5fd74eeab
joshhaws <joshhaws0@gmail.com>
2017-04-10 14:37:01 -0600

    fix

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,7 +1,7 @@
 class AccountTransaction < ApplicationRecord
 
   def clean_trans_date
-    transaction_date.to_date.to_formatted_s(:rfc822)
+    date.to_date.to_formatted_s(:rfc822)
   end
 
   def account_name
